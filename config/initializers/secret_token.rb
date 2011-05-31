aff594f120e69a4148c953a305c7bad0cb262eb2
Mark Bennett <mark@burmis.ca>
2011-05-30 19:30:42 -0600

    Added rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MattsPlants::Application.config.secret_token = 'c3c75f9bf3a898a62e5feeb8476bd08e9e36ae60d879b2a3ee43a84e42489ff8744f5a8dc462dfd3552daecc24852e24b5aaf76dbaa576382903d86ff7563e7c'
